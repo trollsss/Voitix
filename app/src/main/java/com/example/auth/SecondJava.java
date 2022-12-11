addfd1a5a8f6c04b361834ef9426cd77d8c5a288
trollsss <113868634+trollsss@users.noreply.github.com>
2022-12-11 18:41:03 +0900

    Initial commit

---
+++ app/src/main/java/com/example/auth/SecondJava.java
@@ -0,0 +1,14 @@
+package com.example.auth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SecondJava extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
